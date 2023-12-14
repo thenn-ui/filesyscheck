9b4fafab7b3a7eea28d647ec662886fceac7facd
Thennannamalai Malligarjunan <txm230003@csjaws.utdallas.edu>
2023-12-14 09:53:34 -0600

    added check for first case (NOT TESTED)

--- xv6/fcheck.c
+++ xv6/fcheck.c
@@ -14,6 +14,24 @@
 
 #define BLOCK_SIZE (BSIZE)
 
+#define COLOR_BOLD	"\e[1m"
+#define COLOR_OFF	"\e[m"
+#define ENDL		"\n"
+
+
+#define T_DIR  1   // Directory
+#define T_FILE 2   // File
+#define T_DEV  3   // Special device
+
+
+void throwerr(char *string)
+{
+	char buf[256];
+	sprintf(buf, "ERROR: %s\n", string);
+	fprintf(stderr, buf);
+	exit(1);
+}
+
 
 int
 main(int argc, char *argv[])
@@ -23,21 +41,25 @@ main(int argc, char *argv[])
   struct dinode *dip;
   struct superblock *sb;
   struct dirent *de;
+  struct stat statbuf;
 
   if(argc < 2){
-    fprintf(stderr, "Usage: sample fs.img ...\n");
+    fprintf(stderr, "Usage: fcheck <file_system_image>\n");
     exit(1);
   }
-
 
   fsfd = open(argv[1], O_RDONLY);
   if(fsfd < 0){
-    perror(argv[1]);
+    fprintf(stderr, "image not found\n");
     exit(1);
   }
 
-  /* Dont hard code the size of file. Use fstat to get the size */
-  addr = mmap(NULL, 524248, PROT_READ, MAP_PRIVATE, fsfd, 0);
+  /*DONE: Dont hard code the size of file. Use fstat to get the size */
+  assert(fstat(fsfd, &statbuf) == 0);
+
+  int filesize = statbuf.st_size;
+
+  addr = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fsfd, 0);
   if (addr == MAP_FAILED){
 	perror("mmap failed");
 	exit(1);
@@ -63,6 +85,24 @@ main(int argc, char *argv[])
  	printf(" inum %d, name %s ", de->inum, de->name);
   	printf("inode  size %d links %d type %d \n", dip[de->inum].size, dip[de->inum].nlink, dip[de->inum].type);
   }
+
+  printf("============ check started ================\n");
+
+  printf(" size of struct dinode - %d\n", sizeof(struct dinode));
+  // loops through the inode BLOCKS
+  for(i = 0; i < sb->ninodes; i++)
+  {
+	//printf("type - %d, size - %d, links - %d, addrs - %p, inode num - %d, block offset - %d\n", dip[i].type, dip[i].size, dip[i].nlink, dip[i].addrs, i, (char*)dip - addr);
+	//
+	
+	if((dip[i].type != 0 && (dip[i].type != T_DIR || dip[i].type != T_FILE || dip[i].type != T_DEV)))
+	{
+		throwerr("bad inode");
+	}
+  }
+
+
+
   exit(0);
 
 }
